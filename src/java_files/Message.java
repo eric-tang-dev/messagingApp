dc375e8813143e5d65f56b09fa4b20c816e7ef8e
Eric Tang <tang695@purdue.edu>
2024-11-17 15:09:35 -0500

    Delete src/java_files/Message.java

--- src/java_files/Message.java
+++
@@ -1,9 +0,0 @@
-package java_files;
-
-public interface Message {
-    String getUserKey();
-    int getHistoryMarker();
-    String getContent();
-    boolean getIsLiked();
-    String toString();
-}
